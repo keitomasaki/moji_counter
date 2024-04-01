1954cfc38853a3da60397fb7b227100486b374fe
keitomasaki <keitomasaki@Icloud.com>
2024-04-02 01:37:36 +0900

    最低限の機能が完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import PageDescriptionBox from '../components/topPage/PageDescriptionBox'
 import AppDescriptionBox from '../components/topPage/AppDescriptionBox';
+import ActionArea from '../components/topPage/ActionArea';
 
 const Home = () => {
   return (
     <div>
       <PageDescriptionBox />
       <AppDescriptionBox />
+      <ActionArea />
     </div>
   );
 };
