c42c48a00007358aa64505a4a8bdd7a143194db7
keitomasaki <keitomasaki@Icloud.com>
2024-04-02 00:08:04 +0900

    デプロイを試すためのコミット

--- components/topPage/AppDescriptionBox/index.tsx
+++ components/topPage/AppDescriptionBox/index.tsx
@@ -5,7 +5,7 @@ const AppDescriptionBox = () => {
   return <div className={styles.wrapper}>
     <h2 className={styles.title}>【使い方】</h2>
     <p className={styles.description}>字数をカウントしたい文章は下の欄に入力し、「字数を数える」ボタンをクリックしてください。</p>
-    <p className={styles.description}>最初の状態に戻したい場合は「リセット」ボタンをクリックしてください。</p>
+    <p className={styles.description}>最初の状態に戻したい場合は「リセット」ボタンをクリックしてください。.</p>
   </div>;
 };
 
